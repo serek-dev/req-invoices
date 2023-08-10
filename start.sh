d96634a49a2c6a6c122390546a7c504b2e4e87eb
Sebastian Twarog <sebastian.twarog1989@gmail.com>
2023-08-10 23:07:34 +0200

    Fixed start to include migrations and seed from custom namespace for local dev env purposes

--- start.sh
+++ start.sh
@@ -4,3 +4,6 @@ cd ./docker/
 docker compose up -d
 docker compose run workspace composer install
 docker compose run workspace cp -n .env.example .env
+
+docker compose run workspace php artisan migrate:refresh
+docker compose run workspace php artisan db:seed --class=\\App\\Modules\\Invoices\\Infrastructure\\Database\\Seeders\\DatabaseSeeder
