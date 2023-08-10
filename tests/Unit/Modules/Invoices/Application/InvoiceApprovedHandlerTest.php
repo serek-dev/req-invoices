36a084ca77c60b8cf2d27831498ba8f07fc26a37
Sebastian Twarog <sebastian.twarog1989@gmail.com>
2023-08-10 23:19:42 +0200

    Outlines for test of InvoiceApprovedHandler

--- tests/Unit/Modules/Invoices/Application/InvoiceApprovedHandlerTest.php
+++ tests/Unit/Modules/Invoices/Application/InvoiceApprovedHandlerTest.php
@@ -17,4 +17,35 @@ final class InvoiceApprovedHandlerTest extends TestCase
         $sut = new InvoiceApprovedHandler();
         $this->assertInstanceOf(InvoiceApprovedHandler::class, $sut);
     }
+
+    public function testHandleNonExistingInvoiceThrowsError(): void
+    {
+        // Given I have an event with non-existing uuid
+
+        // Then I should see an error
+
+        // When I handle it
+    }
+
+    public function testHandleOnInvalidStatusThrowsError(): void
+    {
+        // Given I have an existing event
+
+        // But it's not draft status
+
+        // Then I should see an error
+
+        // When I handle it
+    }
+
+    public function testHandle(): void
+    {
+        // Given I have an existing event
+
+        // And it is in draft status
+
+        // Then it should be persisted correctly
+
+        // When I handle it
+    }
 }
